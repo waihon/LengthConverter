fb55c7f744603fe4985b797ec0af5eab0bde0934
Waihon Yew <yewwaihon@gmail.com>
2021-05-20 18:56:43 +0800

    Initial Commit

---
+++ LengthConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthConverter
+//
+//  Created by Yew Wai Hon on 20/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
